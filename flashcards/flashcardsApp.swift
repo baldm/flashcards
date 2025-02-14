26e504e6568d3e1c2e0092d54d79140cb98b6242
baldm <jezo@monta.com>
2025-02-14 12:38:39 +0100

    Initial Commit

---
+++ flashcards/flashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  flashcardsApp.swift
+//  flashcards
+//
+//  Created by Jonathan Zørn on 14/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct flashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ flashcardsTests/flashcardsTests.swift
@@ -0,0 +1,17 @@
+//
+//  flashcardsTests.swift
+//  flashcardsTests
+//
+//  Created by Jonathan Zørn on 14/02/2025.
+//
+
+import Testing
+@testable import flashcards
+
+struct flashcardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
